15a631c84f7da05cdaebba1151681a01c83f0c02
Connor van Spronssen <info@dobefu.com>
2024-10-05 11:26:51 +0200

    Add a main element to the layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
-  <LayoutHeader />
   <NuxtRouteAnnouncer />
-  <NuxtPage />
+
+  <LayoutHeader />
+
+  <main
+    class="mx-auto w-full max-w-7xl px-8"
+    id="main-content"
+  >
+    <NuxtPage />
+  </main>
 </template>
