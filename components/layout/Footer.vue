4312aa5a36f995f95bad5397a50d1fd844ac5e1d
Connor van Spronssen <info@dobefu.com>
2024-10-05 11:49:53 +0200

    Add a sticky footer

---
+++ components/layout/Footer.vue
@@ -0,0 +1,3 @@
+<template>
+  <footer class="border-t p-4 shadow-inner">FOOTER</footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,28 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: "en",
+    class: "h-full",
+  },
+  bodyAttrs: {
+    class: "min-h-full flex flex-col",
+  },
+})
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
 
-  <LayoutHeader />
+  <div class="flex flex-1 flex-col justify-between gap-4">
+    <LayoutHeader />
 
-  <main
-    class="mx-auto w-full max-w-7xl px-8"
-    id="main-content"
-  >
-    <NuxtPage />
-  </main>
+    <main
+      class="mx-auto w-full max-w-7xl flex-1 px-8"
+      id="main-content"
+    >
+      <NuxtPage />
+    </main>
+
+    <LayoutFooter />
+  </div>
 </template>
