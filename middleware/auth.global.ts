6d7158c18d69ddad743bd18a513221affdeb93f1
Connor van Spronssen <info@dobefu.com>
2024-10-05 21:15:19 +0200

    Add some cookie options when changing the token

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,7 +1,12 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   if (import.meta.client) return
 
-  const token = useCookie("auth-token")
+  const token = useCookie("auth-token", {
+    httpOnly: true,
+    expires: new Date(Date.now() + 864e5),
+    path: "/",
+    sameSite: "lax",
+  })
 
   const result = await $fetch("/api/v1/refresh-token", {
     method: "POST",
