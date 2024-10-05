0cf048eaefb72b8a60f884e4c7401ec03245dd63
Connor van Spronssen <info@dobefu.com>
2024-10-05 16:17:00 +0200

    Add a placeholder form to create a temporary user

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const name = ref("")
+
+const createTmpAccount = () => {
+  console.log(1)
+}
+</script>
+
+<template>
+  <TypographyHeading type="h1">
+    {{ $config.public.siteName }}
+  </TypographyHeading>
+
+  <form
+    class="mx-auto my-8 flex w-full max-w-2xl flex-col gap-8 rounded-2xl border border-gray-200 bg-white p-8 shadow-md max-sm:px-4 dark:border-gray-800 dark:bg-gray-900"
+    @submit.prevent="createTmpAccount"
+  >
+    <TypographyHeading
+      type="h2"
+      class="text-center"
+    >
+      {{ $config.public.siteName }}
+    </TypographyHeading>
+
+    <FormInputGroup>
+      <FormLabel>Your name</FormLabel>
+
+      <FormInput
+        autocomplete="name"
+        type="text"
+        name="name"
+        placeholder="John Doe"
+        v-model="name"
+        required
+      />
+    </FormInputGroup>
+
+    <FormButton
+      type="submit"
+      variant="primary"
+      class="m-auto"
+    >
+      Create a poker room
+    </FormButton>
+  </form>
+</template>
