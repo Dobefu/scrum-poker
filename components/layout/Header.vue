bad617a5179f8c014c236b7bd80dd80acfa0e4e6
Connor van Spronssen <info@dobefu.com>
2024-10-06 14:25:05 +0200

    Make the home link external, to bypass the Vue router

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -4,6 +4,7 @@
       class="rounded-full border border-gray-100 bg-white py-4 shadow-md dark:border-gray-950 dark:bg-gray-900"
     >
       <NuxtLink
+        :external="true"
         class="flex items-center gap-4 text-lg font-medium text-gray-800 dark:text-gray-100"
         to="/"
       >
