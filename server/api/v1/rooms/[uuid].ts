a7ffb4ed7874cbd6c62c522bceff0cb4c4dd17dc
Connor van Spronssen <connor.van.spronssen@iodigital.com>
2024-10-08 20:46:16 +0200

    Add logic to remember user data when a user reconnects

--- server/api/v1/rooms/[uuid].ts
+++ server/api/v1/rooms/[uuid].ts
@@ -5,6 +5,7 @@ import { UserData } from "~/types/user-data"
 
 const estimateRedactedString = "<HIDDEN>"
 
+const currentUserData: UserData = {}
 const userData: UserData = {}
 const { db } = useDatabase()
 let roomSettings: (typeof rooms)["$inferSelect"] | undefined
@@ -38,26 +39,27 @@ export default defineWebSocketHandler({
 
       const user = usersWithToken[0]
 
-      if (!(user.id in Object.keys(userData))) {
-        userData[peer.toString()] = {
+      if (!(user.id in Object.keys(currentUserData))) {
+        currentUserData[peer.toString()] = {
+          ...Object.values(userData).find((entry) => entry.user.id === user.id),
           user: user as UserData[0]["user"],
         }
       }
 
-      peer.send({ user: "server", type: "init", data: userData })
+      peer.send({ user: "server", type: "init", data: currentUserData })
       peer.send({ user: "server", type: "roomSettings", data: roomSettings })
 
       peer.publish("poker", {
         user: peer.toString(),
         type: "join",
-        data: userData[peer.toString()],
+        data: currentUserData[peer.toString()],
       })
 
       return
     }
 
     if (payload.type === "estimate") {
-      userData[peer.toString()].estimate =
+      currentUserData[peer.toString()].estimate =
         payload.data as UserData[0]["estimate"]
 
       peer.publish("poker", {
@@ -90,7 +92,7 @@ export default defineWebSocketHandler({
 
       const user = usersWithToken[0]
 
-      const peerId = userData[peer.toString()].user.id
+      const peerId = currentUserData[peer.toString()].user.id
       if (peerId !== user.id) return
 
       db.update(rooms)
@@ -103,9 +105,7 @@ export default defineWebSocketHandler({
       peer.send({ user: "server", type: "toggleCardVisibility" })
       peer.publish("poker", { user: "server", type: "toggleCardVisibility" })
 
-      Object.entries(userData).forEach(([uuid, data]) => {
-        console.log(userData.estimate)
-
+      Object.entries(currentUserData).forEach(([uuid, data]) => {
         peer.send({
           user: uuid,
           type: "estimate",
@@ -130,14 +130,20 @@ export default defineWebSocketHandler({
 
     const msg = {
       user: peer.toString(),
-      data: userData.toString(),
+      data: currentUserData.toString(),
     }
 
     peer.send(msg)
     peer.publish("poker", msg)
   },
   close(peer) {
-    delete userData[peer.toString()]
+    const peerUuid = (Object.entries(userData).find(
+      ([_, entry]) =>
+        entry.user.id === currentUserData[peer.toString()].user.id,
+    ) ?? [peer.toString()])[0]
+
+    userData[peerUuid] = { ...currentUserData[peer.toString()] }
+    delete currentUserData[peer.toString()]
 
     peer.publish("poker", {
       user: "server",
