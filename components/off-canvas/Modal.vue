d320b6cb6da1f85b7982cad8e86ff93299568cfb
Connor van Spronssen <info@dobefu.com>
2024-10-15 14:42:04 +0200

    Add a modal component

---
+++ components/off-canvas/Modal.vue
@@ -0,0 +1,151 @@
+<script setup lang="ts">
+import { onKeyDown } from "@vueuse/core"
+import { useFocusTrap } from "@vueuse/integrations/useFocusTrap"
+import { twMerge } from "tailwind-merge"
+import { nextTick, ref, useSlots, type HTMLAttributes } from "vue"
+
+const props = withDefaults(
+  defineProps<{
+    class?: HTMLAttributes["class"]
+  }>(),
+  {
+    class: undefined,
+  },
+)
+
+const emit = defineEmits(["onClose"])
+
+const slots = useSlots()
+
+const isOpen = ref(false)
+const isInnerOpen = ref(false)
+
+const modalContainerRef = ref(null)
+const { activate: ftActivate, deactivate: ftDeactivate } =
+  useFocusTrap(modalContainerRef)
+
+onKeyDown("Escape", () => {
+  if (!isOpen.value) return
+
+  close()
+})
+
+function open() {
+  document.body.classList.add("overflow-hidden")
+  isOpen.value = true
+}
+
+function close() {
+  isInnerOpen.value = false
+}
+
+async function onModalOpened() {
+  isInnerOpen.value = true
+
+  await nextTick()
+
+  /* v8 ignore start */
+  if (process.env.NODE_ENV !== "test") ftActivate()
+  /* v8 ignore stop */
+}
+
+async function onModalClosed() {
+  isInnerOpen.value = false
+  document.body.classList.remove("overflow-hidden")
+
+  await nextTick()
+
+  /* v8 ignore start */
+  if (process.env.NODE_ENV !== "test") ftDeactivate()
+  /* v8 ignore stop */
+
+  emit("onClose")
+}
+
+defineExpose({ open, close })
+</script>
+
+<template>
+  <Teleport to="body">
+    <Transition
+      enter-active-class="transition-[opacity,backdrop-filter]"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100 backdrop-blur-sm"
+      leave-active-class="transition-[opacity,backdrop-filter]"
+      leave-from-class="opacity-100 backdrop-blur-sm"
+      leave-to-class="opacity-0 backdrop-blur-none"
+      @after-enter="onModalOpened()"
+      @after-leave="onModalClosed()"
+    >
+      <div
+        v-if="isOpen"
+        ref="modalContainerRef"
+        class="fixed inset-0 z-50"
+      >
+        <div
+          class="fixed inset-0 bg-black/20 backdrop-blur-sm"
+          @click="close"
+        />
+
+        <div
+          class="fixed inset-0 mx-auto flex w-full max-w-2xl flex-col justify-center px-8 max-sm:p-2 max-sm:px-4"
+          @click.self="close"
+        >
+          <Transition
+            appear
+            enter-active-class="transition-[opacity,transform]"
+            enter-from-class="-translate-y-2 opacity-0"
+            enter-to-class="translate-y-0 opacity-100"
+            leave-active-class="transition-[opacity,transform]"
+            leave-from-class="translate-y-0 opacity-100"
+            leave-to-class="-translate-y-2 opacity-0"
+            @after-leave="isOpen = false"
+          >
+            <UiCard
+              v-if="isInnerOpen"
+              class="max-h-full flex-1 overflow-auto"
+              :wrapper-class="
+                twMerge(
+                  'flex flex-col rounded-2xl shadow-lg max-sm:h-full',
+                  props.class,
+                )
+              "
+            >
+              <template #header>
+                <div class="flex items-center justify-between font-medium">
+                  <slot
+                    v-if="slots.title"
+                    name="title"
+                  />
+                  <div v-else />
+
+                  <button
+                    class="-m-3 rounded-full p-3 transition-transform hover:scale-125"
+                    @click="close"
+                  >
+                    <Icon
+                      class="size-6"
+                      name="mdi:close"
+                      ssr
+                    />
+                  </button>
+                </div>
+              </template>
+
+              <div>
+                <slot />
+              </div>
+
+              <template
+                v-if="slots.actions"
+                #footer
+              >
+                <slot name="actions" />
+              </template>
+            </UiCard>
+          </Transition>
+        </div>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
---
+++ components/ui/Card.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import { twMerge } from "tailwind-merge"
+import { type HTMLAttributes, computed, useSlots } from "vue"
+
+const props = withDefaults(
+  defineProps<{
+    align?: "start" | "middle" | "end"
+    class?: HTMLAttributes["class"]
+    size?: "lg" | "md" | "sm"
+    wrapperClass?: HTMLAttributes["class"]
+  }>(),
+  {
+    align: undefined,
+    class: undefined,
+    size: "md",
+    wrapperClass: undefined,
+  },
+)
+
+const slots = useSlots()
+
+const alignClass = computed(() => {
+  switch (props.align) {
+    case "middle":
+      return "text-center"
+    case "end":
+      return "text-end"
+    default:
+      return "text-start"
+  }
+})
+
+const borderRadius = computed(() => {
+  switch (props.size) {
+    case "lg":
+      return "rounded-2xl"
+    case "sm":
+      return "rounded-lg"
+    default:
+      return "rounded-xl"
+  }
+})
+
+const padding = computed(() => {
+  switch (props.size) {
+    case "lg":
+      return "px-7 py-7"
+    case "sm":
+      return "px-4 py-4"
+    default:
+      return "px-6 py-6"
+  }
+})
+
+const paddingFooterHeader = computed(() => {
+  switch (props.size) {
+    case "lg":
+      return "py-5"
+    case "sm":
+      return "py-3"
+    default:
+      return "py-4"
+  }
+})
+</script>
+
+<template>
+  <div
+    :class="
+      twMerge(
+        'overflow-hidden border border-gray-200 bg-white shadow dark:border-gray-800 dark:bg-gray-950',
+        alignClass,
+        borderRadius,
+        props.wrapperClass,
+      )
+    "
+    data-test-id="wrapper"
+  >
+    <div
+      v-if="slots.header"
+      :class="
+        twMerge(
+          'border-b border-b-gray-200 bg-gray-100 text-gray-600 dark:border-b-gray-800 dark:bg-gray-900 dark:text-gray-200',
+          padding,
+          paddingFooterHeader,
+        )
+      "
+      data-test-id="header"
+    >
+      <slot name="header" />
+    </div>
+
+    <div
+      v-if="slots.default"
+      :class="twMerge('flex flex-col gap-8', padding, props.class)"
+      data-test-id="body"
+    >
+      <slot />
+    </div>
+
+    <div
+      v-if="slots.footer"
+      :class="
+        twMerge(
+          'border-t border-t-gray-200 dark:border-t-gray-800',
+          padding,
+          paddingFooterHeader,
+        )
+      "
+      data-test-id="footer"
+    >
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
