186537500c6e4aa79efc14680ca7e8efc46e502d
Connor van Spronssen <info@dobefu.com>
2024-10-15 07:25:43 +0200

    Add a (hardcoded for now) version to the footer

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer
-    class="border-t border-gray-100 bg-gray-100 p-4 shadow-inner dark:border-gray-900 dark:bg-gray-900"
+    class="border-t border-gray-100 bg-gray-100 p-4 text-center text-gray-500 shadow-inner dark:border-gray-900 dark:bg-gray-900 dark:text-gray-300"
   >
-    FOOTER
+    v0.1.0
   </footer>
 </template>
