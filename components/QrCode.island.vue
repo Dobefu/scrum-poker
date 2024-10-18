88d23635f8f60eca1cb8bb769c42f835c280d752
Connor van Spronssen <info@dobefu.com>
2024-10-18 15:44:23 +0200

    Make the QR code render crisp on hiDPI displays

--- components/QrCode.island.vue
+++ components/QrCode.island.vue
@@ -12,12 +12,15 @@ const code = await QRCode.toDataURL(props.data, {
   type: "image/jpeg",
   margin: 3,
   errorCorrectionLevel: "H",
+  scale: 8,
 })
 </script>
 
 <template>
   <img
     class="rounded-xl border shadow-md dark:border-gray-800"
+    width="206px"
+    height="206px"
     :alt="$props.data"
     :title="$props.data"
     :src="code"
