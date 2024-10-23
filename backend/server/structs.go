d22411e74913c6c2447983ff823698224c3c0853
Connor van Spronssen <info@dobefu.com>
2024-10-23 07:21:35 +0200

    Add a struct for Websocket room settings

---
+++ backend/server/structs.go
@@ -0,0 +1,19 @@
+package server
+
+import "scrumpoker/database"
+
+type RoomSettings struct {
+    ID uint32
+    UUID string
+    Owner uint32
+    Name string
+    Admins []uint32
+    CreatedAt uint32
+    ShowCards bool
+    Cards string
+}
+
+type RoomData struct {
+	RoomSettings RoomSettings
+	Users map[string]database.User
+}
