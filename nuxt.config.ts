73d346b5f40fc3ecf18548e7273204705f12ba36
Connor van Spronssen <info@dobefu.com>
2024-10-27 18:21:53 +0100

    Bump the major version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       siteName: "Scrum Poker",
-      version: "v0.2.0",
+      version: "v1.0.0",
       https: !!process.env.HTTPS,
       wsEndpoint: process.env.WS_ENDPOINT,
     },
