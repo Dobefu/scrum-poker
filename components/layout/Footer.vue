9c66e13e7cbb65598815b66fa7f31fb44a3bfbf1
Connor van Spronssen <info@dobefu.com>
2025-02-24 07:25:23 +0100

    chore: Add a link to the footer

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,7 +1,16 @@
 <template>
   <footer
-    class="border-t border-gray-100 bg-gray-100 p-4 text-center text-gray-600 shadow-inner dark:border-gray-900 dark:bg-gray-900 dark:text-gray-300"
+    class="flex justify-between border-t border-gray-100 bg-gray-100 p-4 text-gray-600 shadow-inner dark:border-gray-900 dark:bg-gray-900 dark:text-gray-300"
   >
-    {{ $config.public.version }}
+    <p>
+      By
+      <a
+        class="text-primary-700 dark:text-primary-500"
+        href="https://cv.connor.nl"
+        target="_blank"
+        >Connor van Spronssen</a
+      >
+    </p>
+    <span>{{ $config.public.version }}</span>
   </footer>
 </template>
